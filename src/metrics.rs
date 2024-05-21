b230e7fe9f5a3cd744fe98744373919549be8d59
sheldonoreoli <sheldonoreoli@gmail.com>
2024-05-21 16:50:44 +0800

    feat: support dashmap

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,12 +1,12 @@
-use std::collections::HashMap;
 use std::fmt;
-use std::sync::{Arc, RwLock};
+use std::sync::Arc;
 
-use anyhow::{anyhow, Result};
+use anyhow::Result;
+use dashmap::DashMap;
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<RwLock<HashMap<String, i64>>>,
+    data: Arc<DashMap<String, i64>>,
 }
 
 // metrics has a new method that creates a new instance of Metrics
@@ -15,38 +15,27 @@ pub struct Metrics {
 impl Metrics {
     pub fn new() -> Metrics {
         Metrics {
-            data: Arc::new(RwLock::new(HashMap::new())),
+            data: Arc::new(DashMap::new()),
         }
     }
 
     pub fn incr(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
-        let count = data.entry(key.into()).or_insert(0);
-        *count += 1;
+        let mut counter = self.data.entry(key.into()).or_insert(0);
+        *counter += 1;
         Ok(())
     }
 
     pub fn decr(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
-        let count = data.entry(key.into()).or_insert(0);
-        *count -= 1;
+        let mut counter = self.data.entry(key.into()).or_insert(0);
+        *counter -= 1;
         Ok(())
-    }
-
-    pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
-        Ok(self
-            .data
-            .read()
-            .map_err(|e| anyhow!(e.to_string()))?
-            .clone())
     }
 }
 
 impl fmt::Display for Metrics {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let data = self.data.read().map_err(|e| fmt::Error {})?;
-        for (key, value) in data.iter() {
-            writeln!(f, "{}: {}", key, value)?;
+        for entry in self.data.iter() {
+            writeln!(f, "{}: {}", entry.key(), entry.value())?;
         }
         Ok(())
     }
