d1f4e3ce1c481184c5be9ee7086c6d5f79bf63ba
sheldonoreoli <sheldonoreoli@gmail.com>
2024-05-21 16:01:37 +0800

    feat: support RwLock

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -21,7 +21,7 @@ fn main() -> Result<()> {
 
     loop {
         thread::sleep(std::time::Duration::from_secs(10));
-        println!("{:?}", metrics.snapshot());
+        println!("{}", metrics);
     }
 }
 
--- src/metrics.rs
+++ src/metrics.rs
@@ -1,11 +1,12 @@
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::fmt;
+use std::sync::{Arc, RwLock};
 
 use anyhow::{anyhow, Result};
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<Mutex<HashMap<String, i64>>>,
+    data: Arc<RwLock<HashMap<String, i64>>>,
 }
 
 // metrics has a new method that creates a new instance of Metrics
@@ -14,19 +15,19 @@ pub struct Metrics {
 impl Metrics {
     pub fn new() -> Metrics {
         Metrics {
-            data: Arc::new(Mutex::new(HashMap::new())),
+            data: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
     pub fn incr(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
         let count = data.entry(key.into()).or_insert(0);
         *count += 1;
         Ok(())
     }
 
     pub fn decr(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
         let count = data.entry(key.into()).or_insert(0);
         *count -= 1;
         Ok(())
@@ -35,8 +36,18 @@ impl Metrics {
     pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
         Ok(self
             .data
-            .lock()
+            .read()
             .map_err(|e| anyhow!(e.to_string()))?
             .clone())
+    }
+}
+
+impl fmt::Display for Metrics {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let data = self.data.read().map_err(|e| fmt::Error {})?;
+        for (key, value) in data.iter() {
+            writeln!(f, "{}: {}", key, value)?;
+        }
+        Ok(())
     }
 }
